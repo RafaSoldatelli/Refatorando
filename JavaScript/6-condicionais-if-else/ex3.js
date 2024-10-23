e30885a9ab0f60d2777b33f24e8d87f20a748dc2
Rafael Soldatelli  da Rosa <you@example.com>
2024-10-22 22:35:12 -0300

    Javascrit-modulo2.2

---
+++ JavaScript/6-condicionais-if-else/ex3.js
@@ -0,0 +1,3 @@
+let numero = 5;
+let resultado = numero % 2 === 0 ? "par" : "ímpar";
+console.log(`O número é ${resultado}!`);
---
+++ JavaScript/6-condicionais-if-else/ex4.js
@@ -0,0 +1,9 @@
+let number = -2;
+
+if (number > 0) {
+    console.log("O número é positivo!")
+} else if (number === 0) {
+    console.log("O número é zero!")
+} else if (number < 0) {
+    console.log("O número é negativo!")
+}
