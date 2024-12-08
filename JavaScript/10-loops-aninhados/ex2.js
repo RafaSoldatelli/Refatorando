d18419fa33a9583880756c5851982a32999e974a
Rafael Soldatelli  da Rosa <you@example.com>
2024-12-08 11:15:14 -0300

    Curso JavaScript

---
+++ JavaScript/10-loops-aninhados/ex2.js
@@ -0,0 +1,7 @@
+for (let linha = 1; linha <= 3; linha++) {
+    let desenho = '';
+    for (let coluna = 1; coluna <= linha; coluna++) {
+        desenho += '*';
+    }
+    console.log(desenho);
+}
